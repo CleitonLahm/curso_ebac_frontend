0c72046f849693eca29c9124f349424ba07ec763
Cleiton Lahm <cleijllahm@gmail.com>
2023-06-15 08:08:27 -0300

    ad

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+
+function compilaSass() {
+  return gulp.src('./source/styles/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./build/styles'));
+}
+
+exports.compilaSass = compilaSass()
