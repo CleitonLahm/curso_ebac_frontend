4cd1dd1dc6f4b4f51f0a899d4effded915e27a5f
Cleiton Lahm <cleijllahm@gmail.com>
2023-08-03 13:03:34 -0300

    ex finalizado

---
+++ src/script.js
@@ -0,0 +1,17 @@
+
+const alunos = [
+  { nome: 'João', nota: 7 },
+  { nome: 'Maria', nota: 5 },
+  { nome: 'Pedro', nota: 8 },
+  { nome: 'Ana', nota: 6 },
+  { nome: 'Paula', nota: 9 },
+];
+
+
+function filtrarAlunosAprovados(alunos) {
+  return alunos.filter((aluno) => aluno.nota >= 6);
+}
+
+
+const alunosAprovados = filtrarAlunosAprovados(alunos);
+console.log(alunosAprovados);
